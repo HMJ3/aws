43dbc8ebea090a1cda80a81732a7b6dd64ec2e53
HMJ3 <henrik.einola@gmail.com>
2026-03-23 19:26:04 +0200

    Updated naming convention

---
+++ launch.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Create Instance Profile 
+
+aws iam create-instance-profile \
+    --instance-profile-name lab-instance-profile-webserver
+
+# Add Role To Profile
+
+aws iam add-role-to-instance-profile \
+    --role-name LabRole \
+    --instance-profile-name lab-instance-profile-webserver
+
+# Create S3 Bucket
+
+BUCKET_NAME="web-bucket-$(openssl rand -hex 6)"
+echo "Bucket name: $BUCKET_NAME"
+
+aws s3api create-bucket \
+    --bucket $BUCKET_NAME \
+    --region us-east-1
+
+# Upload index.html to Bucket
+
+aws s3 cp setup-files/website/index.html s3://$BUCKET_NAME/index.html \
+    --content-type "text/html"
+
+# Configure Web Server
+
+# Get VPC ID
+VPC_ID=$(aws ec2 describe-vpcs \
+    --query "Vpcs[].VpcId" \
+    --output text)
+echo "VPC ID: $VPC_ID"
+
+# Create Security Group
+aws ec2 create-security-group \
+    --group-name web-server-sg \
+    --description "security group for web server" \
+    --vpc-id $VPC_ID
+
+# Get Security Group ID
+SG_ID=$(aws ec2 describe-security-groups \
+    --filters "Name=group-name,Values=web-server-sg" \
+    --query 'SecurityGroups[0].GroupId' \
+    --output text)
+echo "Security Group ID: $SG_ID"
+
+# Allow HTTP traffic
+aws ec2 authorize-security-group-ingress \
+    --group-id $SG_ID \
+    --protocol tcp \
+    --port 80 \
+    --cidr 0.0.0.0/0
+
+# Launch Web Server
+
+aws ec2 run-instances \
+    --image-id ami-02dfbd4ff395f2a1b \
+    --count 1 \
+    --instance-type t3.micro \
+    --region us-east-1 \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=web-server}]' \
+    --security-group-ids $SG_ID \
+    --key-name vockey \
+    --iam-instance-profile Name=lab-instance-profile-webserver \
+    --user-data file://setup-files/nginx-install.sh
